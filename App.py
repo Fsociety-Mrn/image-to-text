cf5d36e1b7cf4e04f961b08559f3579c742c245f
Fsociety-Mrn <lisboamillen30@gmail.com>
2024-12-24 19:08:01 +0800

    fixing docker images

--- App.py
+++ App.py
@@ -11,8 +11,4 @@ app.register_blueprint(pages)
 
 
 if __name__ == '__main__':
-    app.run(
-        # host='192.168.100.134',
-        host='0.0.0.0',
-        debug=True,
-        port=1000) 
+    app.run(debug=True) 
--- Dockerfile
+++ Dockerfile
@@ -48,4 +48,4 @@ COPY . .
 EXPOSE 8000
 
 # Run the application.
-CMD gunicorn 'App:app' --bind=0.0.0.0:8000
+CMD flask run --host=0.0.0.0
